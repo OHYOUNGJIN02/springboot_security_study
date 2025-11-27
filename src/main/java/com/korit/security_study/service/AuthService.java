2123ab8c45e19ba88fdd8d9685c18f5c477b3df6
dongyoon7212 <dongyoon7212@naver.com>
2025-11-27 16:42:00 +0900

    Init: first commit

---
+++ src/main/java/com/korit/security_study/service/AuthService.java
@@ -0,0 +1,71 @@
+package com.korit.security_study.service;
+
+import com.korit.security_study.dto.ApiRespDto;
+import com.korit.security_study.dto.SigninReqDto;
+import com.korit.security_study.dto.SignupReqDto;
+import com.korit.security_study.entity.User;
+import com.korit.security_study.entity.UserRole;
+import com.korit.security_study.repository.UserRepository;
+import com.korit.security_study.repository.UserRoleRepository;
+import com.korit.security_study.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserRoleRepository userRoleRepository;
+
+    public ApiRespDto<?> signup(SignupReqDto signupReqDto) {
+        //username 중복검사
+        Optional<User> foundUser = userRepository.getUserByUsername(signupReqDto.getUsername());
+        if (foundUser.isPresent()) {
+            return new ApiRespDto<>("failed", "이미 존재하는 아이디 입니다.", null);
+        }
+        //추가
+        Optional<User> optionalUser = userRepository.addUser(signupReqDto.toEntity(bCryptPasswordEncoder));
+        //role 추가
+        UserRole userRole = UserRole.builder()
+                .userId(optionalUser.get().getUserId())
+                .roleId(3)
+                .build();
+        userRoleRepository.addUserRole(userRole);
+        return new ApiRespDto<>("success", "회원가입이 완료되었습니다.", optionalUser.get());
+    }
+
+    public ApiRespDto<?> signin(SigninReqDto signinReqDto) {
+        //username을 가진 정보가 있는지 조회
+        Optional<User> foundUser = userRepository.getUserByUsername(signinReqDto.getUsername());
+        if (foundUser.isEmpty()) {
+            return new ApiRespDto<>("failed", "사용자 정보를 다시 확인해주세요.", null);
+        }
+        User user = foundUser.get();
+        if (!bCryptPasswordEncoder.matches(signinReqDto.getPassword(), user.getPassword())) {
+            return new ApiRespDto<>("failed", "사용자 정보를 다시 확인해주세요.", null);
+        }
+
+        String token = jwtUtils.generateAccessToken(user.getUserId().toString());
+        return new ApiRespDto<>("success", "로그인 성공", token);
+    }
+
+    public ApiRespDto<?> getUserByUsername(String username) {
+        Optional<User> foundUser = userRepository.getUserByUsername(username);
+        if (foundUser.isEmpty()) {
+            return new ApiRespDto<>("failed", "존재하지 않은 회원입니다.", null);
+        }
+        return new ApiRespDto<>("success", "회원조회 완료", foundUser.get());
+    }
+}
