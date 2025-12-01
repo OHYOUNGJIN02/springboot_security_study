a3a89213bda1bd88bef3a911c62e85f1ebe59169
dongyoon7212 <dongyoon7212@naver.com>
2025-12-01 12:57:15 +0900

    Feat: oauth2

---
+++ src/main/java/com/korit/security_study/security/handler/OAuth2SuccessHandler.java
@@ -0,0 +1,26 @@
+package com.korit.security_study.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        //OAuth2User 정보를 파싱
+        DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+        String provider = defaultOAuth2User.getAttribute("provider");
+        String providerUserId = defaultOAuth2User.getAttribute("providerUserId");
+        String email = defaultOAuth2User.getAttribute("email");
+        System.out.println(provider);
+        System.out.println(providerUserId);
+        System.out.println(email);
+    }
+}
---
+++ src/main/java/com/korit/security_study/service/OAuth2AuthService.java
@@ -0,0 +1,8 @@
+package com.korit.security_study.service;
+
+import org.springframework.stereotype.Service;
+
+//oauth2로 회원가입 또는 연동
+@Service
+public class OAuth2AuthService {
+}
